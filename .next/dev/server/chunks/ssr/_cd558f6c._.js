fdcc1792393639f493748d6b86f6bdb3456e8db5
nikhilnairwork <nikhilnairwork27@gmail.com>
2026-04-04 17:02:52 +0530

    change

--- .next/dev/server/chunks/ssr/_cd558f6c._.js
+++ .next/dev/server/chunks/ssr/_cd558f6c._.js
@@ -110,7 +110,7 @@ function Header() {
                                 className: "flex items-center gap-3 group",
                                 children: [
                                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                                        className: "relative h-12 w-12 overflow-hidden rounded-xl bg-slate-50 p-1 shadow-sm border border-slate-100 group-hover:scale-105 transition-transform duration-300",
+                                        className: "relative h-12 w-12 overflow-hidden rounded-xl bg-slate-50  shadow-sm border border-slate-100 group-hover:scale-105 transition-transform duration-300",
                                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
                                             src: "/logo.png",
                                             alt: "Euro Tech EDU Logo",
--- .next/dev/static/chunks/_9e3b04d9._.js
+++ .next/dev/static/chunks/_9e3b04d9._.js
@@ -121,7 +121,7 @@ function Header() {
                                 className: "flex items-center gap-3 group",
                                 children: [
                                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                                        className: "relative h-12 w-12 overflow-hidden rounded-xl bg-slate-50 p-1 shadow-sm border border-slate-100 group-hover:scale-105 transition-transform duration-300",
+                                        className: "relative h-12 w-12 overflow-hidden rounded-xl bg-slate-50  shadow-sm border border-slate-100 group-hover:scale-105 transition-transform duration-300",
                                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
                                             src: "/logo.png",
                                             alt: "Euro Tech EDU Logo",
